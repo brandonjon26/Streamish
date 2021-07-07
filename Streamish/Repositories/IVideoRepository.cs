01a435e0dfcfd1659badb634c5b3b494b306531e
Brandon Hill <brandonjon26@gmail.com>
2021-07-07 13:46:01 -0500

    chapter 3 first commit

---
+++ Streamish/Repositories/IVideoRepository.cs
@@ -0,0 +1,14 @@
+﻿using Streamish.Models;
+using System.Collections.Generic;
+
+namespace Streamish.Repositories
+{
+    internal interface IVideoRepository
+    {
+        public List<Video> GetAll();
+        public Video GetById(int id);
+        public void Add(Video video);
+        public void Update(Video video);
+        public void Delete(int id);
+    }
+}
