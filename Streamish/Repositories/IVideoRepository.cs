07cbe96102a3fbbf2d9992d259fdafc392edc87b
Brandon Hill <brandonjon26@gmail.com>
2021-07-07 15:31:33 -0500

    chapter 3 complete, now on to the exercises

--- Streamish/Repositories/IVideoRepository.cs
+++ Streamish/Repositories/IVideoRepository.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 
 namespace Streamish.Repositories
 {
-    internal interface IVideoRepository
+    public interface IVideoRepository
     {
         public List<Video> GetAll();
         public Video GetById(int id);
+        public List<Video> GetAllWithComments();
         public void Add(Video video);
         public void Update(Video video);
         public void Delete(int id);
