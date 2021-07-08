2e65efc472720438c8c71ef39dc550efc93c04c1
Brandon Hill <brandonjon26@gmail.com>
2021-07-08 11:30:53 -0500

    chapter 3 exercise 1 complete

--- Streamish/Repositories/VideoRepository.cs
+++ Streamish/Repositories/VideoRepository.cs
@@ -68,9 +68,14 @@ namespace Streamish.Repositories
                 using (var cmd = conn.CreateCommand())
                 {
                     cmd.CommandText = @"
-                          SELECT Title, Description, Url, DateCreated, UserProfileId
-                            FROM Video
-                           WHERE Id = @Id";
+                        SELECT v.Id, v.Title, v.Description, v.Url, v.DateCreated, v.UserProfileId,
+
+                            up.Name, up.Email, up.DateCreated AS UserProfileDateCreated,
+                            up.ImageUrl AS UserProfileImageUrl
+                        
+                        FROM Video v 
+                            JOIN UserProfile up ON v.UserProfileId = up.Id
+                        WHERE v.Id = @Id";
 
                     DbUtils.AddParameter(cmd, "@Id", id);
 
@@ -86,7 +91,14 @@ namespace Streamish.Repositories
                             Description = DbUtils.GetString(reader, "Description"),
                             DateCreated = DbUtils.GetDateTime(reader, "DateCreated"),
                             Url = DbUtils.GetString(reader, "Url"),
-                            UserProfileId = DbUtils.GetInt(reader, "UserProfileId"),
+                            UserProfile = new UserProfile()
+                            {
+                                Id = DbUtils.GetInt(reader, "UserProfileId"),
+                                Name = DbUtils.GetString(reader, "Name"),
+                                Email = DbUtils.GetString(reader, "Email"),
+                                DateCreated = DbUtils.GetDateTime(reader, "UserProfileDateCreated"),
+                                ImageUrl = DbUtils.GetString(reader, "UserProfileImageUrl"),
+                            },
                         };
                     }
 
